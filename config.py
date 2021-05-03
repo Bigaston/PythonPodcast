9f97203a0619dbd3024d1f91e2e893782114e11b
Bigaston <bigaston1@gmail.com>
2021-05-03 10:30:27 +0200

    Présentation

--- config.py
+++ config.py
@@ -1,24 +1,16 @@
 podcast = {
-  "title": "Kapsule Pixel",
-  "description": "Micro-trottoir absurde qui essaye de répondre grâces aux passants à des questions que personne ne se pose. Période de publication aléatoire qui dépendra du temps qu'il fait dehors et du nombre de moutons dans le village de Montclar.",
-  "url": "https://kapsule.bigaston.me",
-  "author": "Bigaston",
+  "title": "Muffin Cast",
+  "description": "Un super podcast!",
+  "url": "",
+  "author": "Muffi, the Muffin",
   "lang": "fr",
-  "email": "podcast@bigaston.me",
-  "donation": "https://utip.io/bigaston",
+  "email": "",
+  "donation": "",
   "audio_base": "",
   "itunes_category": "Comedy",
   "itunes_subcategory": "",
-  "itunes_subtitle": "Dans la vie il y a des questions que personnes ne se pose...",
+  "itunes_subtitle": "",
   "itunes_type": "episodic",
   "itunes_explicit": "no",
-  "base_description": """
-Dans la vie il y a des questions que personnes ne se pose...
-
-%%comment%%
-
-📍 Enregistré à... chez moi du coup!  
-📅 Le 17 décembre 2019  
-📻 Voix de l'introduction par Fredoune
-  """
+  "base_description": "%%comment%%"
 }
